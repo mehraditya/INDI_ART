ccc0671932dcc2adfbd249a47403e9e29e02cb42
adityamehracr7@gmail.com <adityamehracr7@gmail.com>
2026-04-13 23:39:56 +0530

    fix: Remove share_api from demo_launch()

--- app.py
+++ app.py
@@ -183,7 +183,6 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=int(os.getenv("PORT", 7860)),
-        share=False,
         show_api=False,
         quiet=False,
     )

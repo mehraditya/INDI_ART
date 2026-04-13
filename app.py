3ca107f513f18660b004a3a5334cc613c91746a2
adityamehracr7@gmail.com <adityamehracr7@gmail.com>
2026-04-13 16:30:23 +0530

    fix: Remove all open=False, set share=True

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=int(os.getenv("PORT", 7860)),
-        share=False,
+        share=True,
         show_api=False,
         quiet=True
     )
